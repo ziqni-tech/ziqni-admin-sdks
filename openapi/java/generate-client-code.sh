bed31183d4dfc3235df061a90520f415cdc00964
vossie <carel.vosloo@bronzegate.com>
2022-07-15 21:35:14 +0100

    Moved the generated sources to the target folder

--- openapi/java/generate-client-code.sh
+++ openapi/java/generate-client-code.sh
@@ -4,7 +4,7 @@
 openapi-generator-cli version 6.0
 SPEC_FILE=../ziqni-admin-api.yml
 CONF_FILE=ziqni-admin-api.config.yml
-GEN_DIR=generated
+GEN_DIR=../../java/target/generated-sources/openapi/src/main/java/com/ziqni/sdk/admin
 FILE=generate-client-code.sh
 
 if [ -z "$1" ]; then
@@ -23,36 +23,28 @@ else
   exit
 fi
 
-if [[ -d "$GEN_DIR" ]]; then
-  echo "$GEN_DIR exists."
-  rm -rf ./generated
-  echo "$GEN_DIR deleted."
-  mkdir generated
-  echo "$GEN_DIR created."
 
-  openapi-generator-cli  generate -g java -i $SPEC_FILE -c $CONF_FILE -o ./generated --additional-properties=asyncNative=true --additional-properties=library=native -t ./templates
+openapi-generator-cli  generate -g java -i $SPEC_FILE -c $CONF_FILE -o ./generated --additional-properties=asyncNative=true --additional-properties=library=native -t ./templates
 
-  # Copy the updated docs
-  rm -rf ../../java/docs
-  mkdir ../../java/docs
-  cp -v ./generated/docs/* ../../java/docs/
+# Copy the updated docs
+rm -rf ../../java/docs
+mkdir ../../java/docs
+cp -v ./generated/docs/* ../../java/docs/
 
-  mkdir -p ../../java/src/generated
-  mkdir -p ../../java/src/generated/java/com/ziqni/sdk/admin/api
-  mkdir -p ../../java/src/generated/java/com/ziqni/sdk/admin/model
+mkdir -p $GEN_DIR
+mkdir -p $GEN_DIR/api
+mkdir -p $GEN_DIR/model
 
-  # Copy the new sources - openapi/java/generated/src/main/java/com/ziqni/sdk/admin/api
-  cp -rf ./generated/src/main/java/com/ziqni/sdk/admin/api/*Ws.java ../../java/src/generated/java/com/ziqni/sdk/admin/api/
+# Copy the new sources - openapi/java/generated/src/main/java/com/ziqni/sdk/admin/api
+cp -rf ./generated/src/main/java/com/ziqni/sdk/admin/api/*Ws.java $GEN_DIR/api/
 
-  # Copy the new sources - openapi/java/generated/src/main/java/com/ziqni/sdk/admin/model
-  cp -rf ./generated/src/main/java/com/ziqni/sdk/admin/model ../../java/src/generated/java/com/ziqni/sdk/admin
+# Copy the new sources - openapi/java/generated/src/main/java/com/ziqni/sdk/admin/model
+cp -rf ./generated/src/main/java/com/ziqni/sdk/admin/model $GEN_DIR
 
-  cp -rf ./generated/src/main/java/com/ziqni/sdk/admin/ZiqniAdminApiFactory.java ../../java/src/generated/java/com/ziqni/sdk/admin
-  cp -rf ./generated/src/main/java/com/ziqni/sdk/admin/ApiException.java ../../java/src/generated/java/com/ziqni/sdk/admin
-  cp -rf ./generated/src/main/java/com/ziqni/sdk/admin/JSON.java ../../java/src/generated/java/com/ziqni/sdk/admin
-  cp -rf ./generated/src/main/java/com/ziqni/sdk/admin/RFC3339DateFormat.java ../../java/src/generated/java/com/ziqni/sdk/admin
+cp -rf ./generated/src/main/java/com/ziqni/sdk/admin/ZiqniAdminApiFactory.java $GEN_DIR
+cp -rf ./generated/src/main/java/com/ziqni/sdk/admin/ApiException.java $GEN_DIR
+cp -rf ./generated/src/main/java/com/ziqni/sdk/admin/JSON.java $GEN_DIR
+cp -rf ./generated/src/main/java/com/ziqni/sdk/admin/RFC3339DateFormat.java $GEN_DIR
 
-else
-  echo "Directory '<project-root>/openapi/java/$GEN_DIR' not found"
-fi
+
 
