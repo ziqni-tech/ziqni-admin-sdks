edb045775fc9e060e55394f14445dd5b3fb65a0e
GivenTP-ziqni <given@ziqni.com>
2023-08-18 13:23:26 +0200

    adding InstantWinsApiTest

---
+++ java/src/test/java/com/ziqni/admin/sdk/api/InstantWinsApiTest.java
@@ -0,0 +1,10 @@
+package com.ziqni.admin.sdk.api;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestMethodOrder;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class InstantWinsApiTest implements tests.utils.CompleteableFutureTestWrapper {
+}
