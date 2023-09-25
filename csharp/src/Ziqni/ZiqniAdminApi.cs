f4e48982bdc249025ae1523f6a0ccc4546f82b32
vossie <carel.vosloo@bronzegate.com>
2023-09-25 13:43:44 +0100

    Testing cs code

---
+++ csharp/src/Ziqni/ZiqniAdminApi.cs
@@ -0,0 +1,11 @@
+namespace Ziqni
+{
+    public class ZiqniAdminApi
+    {
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+            }
+        }
+}
