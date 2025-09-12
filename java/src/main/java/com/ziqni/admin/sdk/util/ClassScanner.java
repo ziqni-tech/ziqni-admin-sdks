a0c7328687732cf2b9fa2d7b159d2e6db50a59d2
given <given@ziqni.com>
2025-09-12 17:24:53 +0200

    Added a patch to scan components in fat jar

--- java/src/main/java/com/ziqni/admin/sdk/util/ClassScanner.java
+++ java/src/main/java/com/ziqni/admin/sdk/util/ClassScanner.java
@@ -7,9 +7,11 @@ import org.apache.commons.lang3.SystemUtils;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.InputStream;
 import java.lang.reflect.Type;
 import java.util.*;
 import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 import java.util.jar.JarInputStream;
 
 public class ClassScanner {
@@ -64,22 +66,46 @@ public class ClassScanner {
         for (String classpathEntry : classPathEntries) {
             if (classpathEntry.endsWith(".jar")) {
                 File jar = new File(classpathEntry);
-                try {
-                    JarInputStream is = new JarInputStream(new FileInputStream(jar));
+                try (JarFile is = new JarFile(jar)) {
                     JarEntry entry;
-                    while((entry = is.getNextJarEntry()) != null) {
+                    Enumeration<JarEntry> entries = is.entries();
+                    while ((entries.hasMoreElements())) {
+                        entry = entries.nextElement();
                         name = entry.getName();
                         if (SystemUtils.IS_OS_WINDOWS && name.endsWith(".class")) {
-                            if (name.contains(path.replaceAll("\\\\","/"))) {
+                            if (name.contains(path.replaceAll("\\\\", "/"))) {
                                 String classPath = name.substring(0, entry.getName().length() - 6);
                                 classPath = classPath.replaceAll("[\\|/]", ".");
                                 classes.add(Class.forName(classPath));
                             }
                         }
-                        else  if (name.contains(path) && name.endsWith(".class")) {
+                        else if (name.contains(path) && name.endsWith(".class")) {
                             String classPath = name.substring(0, entry.getName().length() - 6);
                             classPath = classPath.replaceAll("[\\|/]", ".");
                             classes.add(Class.forName(classPath));
+                        }
+                        // ✅ extra block: discover nested admin-sdk jar inside BOOT-INF/lib
+                        else if (name.startsWith("BOOT-INF/lib/") && name.endsWith(".jar") && name.contains("admin-sdk")) {
+                            try (InputStream nestedJarStream = Thread.currentThread()
+                                    .getContextClassLoader()
+                                    .getResourceAsStream(name)) {
+                                if (nestedJarStream != null) {
+                                    try (JarInputStream nestedIs = new JarInputStream(nestedJarStream)) {
+                                        JarEntry nestedEntry;
+                                        while ((nestedEntry = nestedIs.getNextJarEntry()) != null) {
+                                            String nestedName = nestedEntry.getName();
+                                            if (nestedName.startsWith("com/ziqni/admin/sdk/model/")
+                                                    && nestedName.endsWith(".class")) {
+                                                String classPath = nestedName.substring(0, nestedName.length() - 6);
+                                                classPath = classPath.replaceAll("[\\|/]", ".");
+                                                classes.add(Class.forName(classPath));
+                                            }
+                                        }
+                                    }
+                                } else {
+                                    System.err.println("Could not open nested admin-sdk jar: " + name);
+                                }
+                            }
                         }
                     }
                 } catch (Exception ex) {
